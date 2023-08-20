cb3fdb0a423544260724e4feb3739ba1a6338874
Klel24 <claudiosvazquez@gmail.com>
2023-08-20 13:02:01 -0300

    create server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require ("express"); //llamo a el modulo express
+const path = require("path"); //llamo al module path para que luego resuelva las rutas
+
+const app = express(); //asigno  la funcion express a una constante
+
+app.use(express.static(path.join(__dirname, "../public"))); //agrego la ruta absolutaa la carpeta publica
+
+const PORT = 3001; //asigno el puerto que voy a usar a una variable para luego usar siempre la variable
+
+app.listen(PORT, () =>{
+    console.log ("Se prendió en el puerto " + PORT);  //prendo el servidor
+});
+
+app.get ("/", (req, res) =>{
+    res.sendFile(path.join(__dirname, "views/home.html")); //le asigno home.html al home
+});
+
+//app.get ("/", (req, res) =>{
+// res.send ("Hola");
+//});
+//Archivos publicos--> Css Estilos; Img: images; Js: front 
+ 
